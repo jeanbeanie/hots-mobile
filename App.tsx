c46e2e6f70c3c7607038b684f390d56063a1da16
jeanbeanie <msjeanneramos@gmail.com>
2020-11-16 14:06:51 -0500

    removed a bunch of default copy/style stuff

--- App.tsx
+++ App.tsx
@@ -1,11 +1,6 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
  * Generated with the TypeScript template
  * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
  */
 
 import React from 'react';
@@ -18,11 +13,7 @@ import {
   StatusBar,
 } from 'react-native';
 
-import {
-  Colors,
-} from 'react-native/Libraries/NewAppScreen';
-
-declare const global: {HermesInternal: null | {}};
+import {Colors} from 'react-native/Libraries/NewAppScreen';
 
 const App = () => {
   return (
@@ -32,11 +23,6 @@ const App = () => {
         <ScrollView
           contentInsetAdjustmentBehavior="automatic"
           style={styles.scrollView}>
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
           <View style={styles.body}>
             <Text>HotsMobile</Text>
           </View>
@@ -56,32 +42,6 @@ const styles = StyleSheet.create({
   },
   body: {
     backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
   },
 });
 
